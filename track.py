00eaf5978ac5a40958656831a9b6048eecd87754
somchaiza001 <107309567+somchaiza001@users.noreply.github.com>
2022-09-22 17:59:34 +0700

    Update track.py

--- track.py
+++ track.py
@@ -1,17 +1,24 @@
 import phonenumbers
-from myphone import number
 from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import timezone
+import os, time, sys
 
+time.sleep(1)
+number = input('Enter your phone number: ')
+
+time.sleep(2)
+print('')
 pepnumber = phonenumbers.parse(number)
 location = geocoder.description_for_number(pepnumber, "en")
 print("Your Country: "+location)
 
+print('')
 service_pro = phonenumbers.parse(number)
 service_loc = carrier.name_for_number(service_pro, "en")
 print("Your Service: "+service_loc)
 
+print('')
 time_zone = phonenumbers.parse(number)
 time = timezone.time_zones_for_number(time_zone)
 print("Time Zone: "+str(time))
